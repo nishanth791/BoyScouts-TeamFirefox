eb06eaf6e6fc443a537db3b5e94dfed9739d280c
tnp6405 <nishanth90@tamu.edu>
2014-12-07 18:34:53 -0600

    run table of database rails admin changes

--- app/models/run.rb
+++ app/models/run.rb
@@ -1,4 +1,119 @@
 class Run < ActiveRecord::Base
   belongs_to :route
   belongs_to :member
+  validates :run_id, :uniqueness=>true
+  validates :route, :presence => true
+  validates :member, :presence => true
+  validates :run_date, :presence => true
+  validates :time_taken, :presence => true
+  validates :am_pm, :presence => true
+  validates :notes, :presence => true
+  
+  after_initialize do
+        if new_record?
+                maxId = Run.find_by_sql("SELECT run_id FROM runs ORDER BY run_idDESC limit 1").first.try(:run_id)
+                self.run_id = maxId+1
+        end
+  end
+
+rails_admin do
+
+   update do
+        field :run_id do
+                label "Run ID"
+                read_only true
+                help 'Required'
+        end
+        field :route do
+                label "Route"
+                help 'Required'
+	end
+        field :member do
+                label "Member"
+                help 'Required'
+	end
+        field :run_date do
+                label "Run date"
+                help 'Required'
+	end
+        field :time_taken do
+                label "Time taken"
+                help 'Required'
+	end
+        field :am_pm do
+                label "Am Pm"
+                help 'Required'
+	end
+        field :notes do
+                label "Notes"
+                help 'Required'
+	end
+  end
+  
+   show do
+        field :run_id do
+                label "Run ID"
+                read_only true
+                help 'Required'
+        end
+        field :route do
+                label "Route"
+                help 'Required'
+	end
+        field :member do
+                label "Member"
+                help 'Required'
+	end
+        field :run_date do
+                label "Run date"
+                help 'Required'
+	end
+        field :time_taken do
+                label "Time taken"
+                help 'Required'
+	end
+        field :am_pm do
+                label "Am Pm"
+                help 'Required'
+	end
+        field :notes do
+                label "Notes"
+                help 'Required'
+	end
+  end
+  
+  create do
+        field :run_id do
+                label "Run ID"
+                read_only true
+                help 'Required'
+        end
+        field :route do
+                label "Route"
+                help 'Required'
+	end
+        field :member do
+                label "Member"
+                help 'Required'
+	end
+        field :run_date do
+                label "Run date"
+                help 'Required'
+	end
+        field :time_taken do
+                label "Time taken"
+                help 'Required'
+	end
+        field :am_pm do
+                label "Am Pm"
+                help 'Required'
+	end
+        field :notes do
+                label "Notes"
+                help 'Required'
+	end
+  end
+  
 end
+end
+
