76d2e58afc77bcc0d3471e0c2d922ec6a904c947
tnp6405 <nishanth90@tamu.edu>
2014-12-07 18:11:59 -0600

    route table of database rails admin changes

--- app/models/route.rb
+++ app/models/route.rb
@@ -2,7 +2,13 @@ class Route < ActiveRecord::Base
   validates :route_id, :uniqueness=>true
   validates :route_name, :presence => true
   validates :print_sequence, :presence => true
-  
+  after_initialize do
+        if new_record?
+                maxId = Route.find_by_sql("SELECT route_id FROM route ORDER BY route_id DESC limit 1").first.try(:route_id)
+                self.route_id = maxId+1
+        end
+  end
+ 
 rails_admin do
 
    update do
@@ -12,11 +18,11 @@ rails_admin do
                 help ''
         end
         field :route_name do
-                label "Route Name"
+                label "Route name"
                 help 'Required'
         end
         field :print_sequence do
-		label "Print Sequence"
+		label "Print sequence"
                 help 'Required'
 	end
    end
@@ -28,11 +34,11 @@ rails_admin do
                 help ''
         end
         field :route_name do
-                label "Route Name"
+                label "Route name"
                 help 'Required'
         end
         field :print_sequence do
-                label "Print Sequence"
+                label "Print sequence"
                 help 'Required'
         end
    end
@@ -43,11 +49,11 @@ rails_admin do
                 help ''
         end
         field :route_name do
-                label "Route Name"
+                label "Route name"
                 help 'Required'
         end
         field :print_sequence do
-                label "Print Sequence"
+                label "Print sequence"
                 help 'Required'
         end
    end
