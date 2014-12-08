6716ee99c77a97e4fd6e7b857e60aed9801c7cbf
tnp6405 <nishanth90@tamu.edu>
2014-12-07 18:04:48 -0600

    route table of database rails admin changes

--- app/models/route.rb
+++ app/models/route.rb
@@ -2,5 +2,56 @@ class Route < ActiveRecord::Base
   validates :route_id, :uniqueness=>true
   validates :route_name, :presence => true
   validates :print_sequence, :presence => true
+  
+rails_admin do
+
+   update do
+        field :route_id do
+                label "Route ID"
+                read_only true
+                help ''
+        end
+        field :route_name do
+                label "Route Name"
+                help 'Required'
+        end
+        field :print_sequence do
+		label "Print Sequence"
+                help 'Required'
+	end
+   end
+   
+   show do 
+	field :route_id do
+                label "Route ID"
+                read_only true
+                help ''
+        end
+        field :route_name do
+                label "Route Name"
+                help 'Required'
+        end
+        field :print_sequence do
+                label "Print Sequence"
+                help 'Required'
+        end
+   end
+   create do
+        field :route_id do
+                label "Route ID"
+                read_only true
+                help ''
+        end
+        field :route_name do
+                label "Route Name"
+                help 'Required'
+        end
+        field :print_sequence do
+                label "Print Sequence"
+                help 'Required'
+        end
+   end
+
+end
 
 end
