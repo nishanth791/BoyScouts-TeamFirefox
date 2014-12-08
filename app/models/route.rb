e245dcedfbcfab4e26eb39db2463c5878409769b
tnp6405 <nishanth90@tamu.edu>
2014-12-07 18:17:11 -0600

    route table of database rails admin changes

--- app/models/route.rb
+++ app/models/route.rb
@@ -6,6 +6,7 @@ class Route < ActiveRecord::Base
         if new_record?
                 maxId = Route.find_by_sql("SELECT route_id FROM routes ORDER BY route_id DESC limit 1").first.try(:route_id)
                 self.route_id = maxId+1
+                self.route_name = "Unassigned"
         end
   end
  
