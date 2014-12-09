5fb91277a78d2ac61503707518ece062f1ed685b
tnp6405 <nishanth90@tamu.edu>
2014-12-08 19:53:14 -0600

    constraints added in members table

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,124 @@
 class Member < ActiveRecord::Base
+  
+  validates :member_id, :uniqueness=>true
+  validates :member_last_name, :presence => true
+  validates :member_first_name, :presence => true
+  validates :member_cell, :presence => true
+  validates :member_landline, :presence => true
+  validates :member_email, :presence => true
+  validates :member_inactive, :presence => true
+  validates :member_vehicle, :presence => true
+  after_initialize do
+        if new_record?
+                maxId = Member.find_by_sql("SELECT member_id FROM members ORDER BY member_id DESC limit 1").first.try(:member_id)
+                self.member_id = maxId+1
+        end
+  end
+ rails_admin do
+   update do
+        field :member_id do
+                label "Member ID"
+                read_only true
+                help ''
+        end
+        field :member_last_name  do
+                label "Member last name"
+                help 'Required'
+        end
+        field :member_first_name do
+                label "Member first name"
+                help 'Required'
+        end
+        field :member_cell do
+                label "Member cell"
+                help 'Required'
+        end
+        field :member_landline do
+                label "Member landline"
+                help 'Required'
+        end
+        field :member_email do
+                label "Member email"
+                help 'Required'
+        end
+        field :member_inactive do
+                label "Member inactive"
+                help 'Required'
+        end
+        field :member_vehicle do
+                label "Member vehicle"
+                help 'Required'
+        end
+   end
+   show do
+        field :member_id do
+                label "Member ID"
+                read_only true
+                help ''
+        end
+        field :member_last_name  do
+                label "Member last name"
+                help 'Required'
+        end
+        field :member_first_name do
+                label "Member first name"
+                help 'Required'
+        end
+        field :member_cell do
+                label "Member cell"
+                help 'Required'
+        end
+        field :member_landline do
+                label "Member landline"
+                help 'Required'
+        end
+        field :member_email do
+                label "Member email"
+                help 'Required'
+        end
+        field :member_inactive do
+                label "Member inactive"
+                help 'Required'
+        end
+        field :member_vehicle do
+                label "Member vehicle"
+                help 'Required'
+        end
+   end
+   create do
+        field :member_id do
+                label "Member ID"
+                read_only true
+                help ''
+        end
+        field :member_last_name  do
+                label "Member last name"
+                help 'Required'
+        end
+        field :member_first_name do
+                label "Member first name"
+                help 'Required'
+        end
+        field :member_cell do
+                label "Member cell"
+                help 'Required'
+        end
+        field :member_landline do
+                label "Member landline"
+                help 'Required'
+        end
+        field :member_email do
+                label "Member email"
+                help 'Required'
+        end
+        field :member_inactive do
+                label "Member inactive"
+                help 'Required'
+        end
+        field :member_vehicle do
+                label "Member vehicle"
+                help 'Required'
+        end
+   end
+ end
 end
