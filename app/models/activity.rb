1267590c566697b207385b230919e701f49821bf
Nilesh Javar <nileshjavar@gmail.com>
2014-12-12 20:31:45 -0600

    check

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -21,9 +21,6 @@ class Activity < ActiveRecord::Base
                 help ''
         end
         field :member do
-		formatted_value do 
-			"Nilesh"
-		end
                 label "Member"
                 help 'Required'
         end
